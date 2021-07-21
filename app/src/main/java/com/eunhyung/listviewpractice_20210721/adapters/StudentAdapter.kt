f1e26658aef60b0973f0943d0bd58bc64a762200
전은형 <ooooo5h91@gmail.com>
2021-07-21 22:18:35 +0900

    inflater변수 추가

--- app/src/main/java/com/eunhyung/listviewpractice_20210721/adapters/StudentAdapter.kt
+++ app/src/main/java/com/eunhyung/listviewpractice_20210721/adapters/StudentAdapter.kt
@@ -1,6 +1,7 @@
 package com.eunhyung.listviewpractice_20210721.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import com.eunhyung.listviewpractice_20210721.datas.Student
 import java.util.*
@@ -9,4 +10,9 @@ class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+//    xml을 코틀린 변수로 꺼내오도록 도와주는 클래스(layoutInflater)
+    val inflater = LayoutInflater.from(mContext)
+
+
 }
