529ede5f2fc6e171b2d7046e5780fd05d0a0b3eb
전은형 <ooooo5h91@gmail.com>
2021-07-21 22:14:31 +0900

    ArrayAdapter가 동작하기 위해 필요한 재료를 받아오고, 넘겨주기 까지

--- app/src/main/java/com/eunhyung/listviewpractice_20210721/adapters/StudentAdapter.kt
+++ app/src/main/java/com/eunhyung/listviewpractice_20210721/adapters/StudentAdapter.kt
@@ -1,7 +1,12 @@
 package com.eunhyung.listviewpractice_20210721.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.eunhyung.listviewpractice_20210721.datas.Student
+import java.util.*
 
-class StudentAdapter : ArrayAdapter<Student>() {
+class StudentAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
 }
