f4693893a2340b0e4dfb463b8144f78f9b423f15
전은형 <ooooo5h91@gmail.com>
2021-07-21 22:32:51 +0900

    어댑터 객체 생성

--- app/src/main/java/com/eunhyung/listviewpractice_20210721/MainActivity.kt
+++ app/src/main/java/com/eunhyung/listviewpractice_20210721/MainActivity.kt
@@ -2,6 +2,7 @@ package com.eunhyung.listviewpractice_20210721
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunhyung.listviewpractice_20210721.adapters.StudentAdapter
 import com.eunhyung.listviewpractice_20210721.datas.Student
 import java.util.*
 
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
 //    학생 목록을 담아둘 ArrayList 변수 추가
     val mStudentList = ArrayList<Student>()
 
+//    xml + 데이터를 조합해서 뿌려주는 Adapter 변수 추가
+//    변수는 미리 만들고 싶은데, 대입은 나중에 해야하는 상황
+    lateinit var mAdapter : StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,9 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add( Student("조경진" , 1988))
         mStudentList.add( Student("임슬기" , 1990))
         mStudentList.add( Student("류준열" , 1983))
+
+//        미뤄뒀던 Adapter의 대입을 진행하자
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
 
     }
 }
